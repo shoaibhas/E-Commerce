be710358e62a46fa4da438824e1681a062e1ff6d
Shoaib Hassan <shoaibhassan1022@gmail.com>
2023-12-24 19:23:40 +0500

    crud operations on admin

--- e-commerce/controller/auth/admin.js
+++ e-commerce/controller/auth/admin.js
@@ -50,13 +50,81 @@ const AdminLoginController = {
       const token = Jwt.sign(data1, process.env.JSON_SECRET, {
         expiresIn: "1h",
       });
-      req.session.token=token;
-      req.session.user=user;
-      req.session.save()
+      req.session.token = token;
+      req.session.user = user;
+      req.session.save();
 
-      return res.status(200).json({ message: `${email} Login Successful`,token,data1 });
+      return res
+        .status(200)
+        .json({ message: `${email} Login Successful`, token, data1 });
     } catch (error) {
       console.log({ message: `some bad happened`, error });
+    }
+  },
+  getall: async (req, res) => {
+    try {
+      const a = await adminModel.findAll();
+      // console.log(a);
+      return res.status(200).json({ message: `Get All Users`, a });
+    } catch (error) {
+      console.log(`some bad`, error);
+    }
+  },
+  getone: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const a = await adminModel.findOne({ where: { id } });
+      if (!a) {
+        return res.status(201).json({ message: `User Not Fount` });
+      }
+      return res.status(200).json({ message: `User Found Whose Id ${id}`, a });
+    } catch (error) {
+      return res.status(201).json({ message: `some bad happened`, error });
+    }
+  },
+  update: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const { firstName, lastName, email, password, cnic, phone, address } =
+        req.body;
+      const user = await adminModel.findOne({ where: { id } });
+      if (!user) {
+        return res.status(201).json({ message: `User Not Found` });
+      }
+      // if(user){
+      //   return res
+      //   .status(400)
+      //   .json({ message: `this email ${email} is already exist..` });
+      // }
+      const hashpassword = await hash(password, 10);
+      (user.firstName = firstName),
+        (user.lastName = lastName),
+        (user.email = email),
+        (user.password = hashpassword),
+        (user.cnic = cnic),
+        (user.phone = phone),
+        (user.address = address);
+      await user.save();
+      return res.status(200).json({ message: `Update Successfully`, user });
+    } catch (error) {
+      return res.status(201).json({ message: `some bad happened`, error });
+    }
+  },
+  // $2b$10$Evn45ahiyEzyde4EV6y8JOtfYh4Dy3BtYTZqSbxl6KCt6ZAAzgXS2,
+  // $2b$10$DG9I5OWXA2Wr5yN7fmwFcuZ77ibEuDvCASuR5z.fGDsFVNWlnb5xq
+  delete: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const user = await adminModel.findOne({ where: { id } });
+      if (!user) {
+        return res
+          .status(201)
+          .json({ message: `This ID ${id} Does Not Exist` });
+      }
+      await user.destroy();
+      return res.status(200).json({ message: `This ID ${id} will be Deleted` });
+    } catch (error) {
+      return res.status(201).json({ message: `Some Bad Happened` });
     }
   },
 };
--- e-commerce/router/admin/index.js
+++ e-commerce/router/admin/index.js
@@ -1,6 +1,15 @@
 import { Router } from "express";
 import AdminLoginController from "../../controller/auth/admin.js";
+import adminLoginValidator from "../../../validation/auth/index.js";
 const adminLoginRouter = Router();
-adminLoginRouter.post("/register", AdminLoginController.register);
+adminLoginRouter.post(
+  "/register",
+  adminLoginValidator.register,
+  AdminLoginController.register
+);
 adminLoginRouter.post("/login", AdminLoginController.login);
+adminLoginRouter.get("/register", AdminLoginController.getall);
+adminLoginRouter.get("/register/:id", AdminLoginController.getone);
+adminLoginRouter.put("/register/:id", AdminLoginController.update);
+adminLoginRouter.delete("/register/:id", AdminLoginController.delete);
 export default adminLoginRouter;
